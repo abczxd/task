ebc853a626a7c4ddb66e86b43b775d5854da2edd
abczxd <kogay_stanislav@mail.ru>
2022-10-19 12:04:49 +0600

    first

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
-$(document).ready(function() {
-    $('.aside__burger').click(function(event) {
+$(document).ready(function () {
+    $('.aside__burger').click(function (event) {
         $('.aside__burger,.aside__menu').toggleClass('active');
         $('body').toggleClass('lock');
     });
+
+    $('.first_btn').click(function (event) {
+        $('.btns__list li').toggleClass('active');
+        $('.btns_back_wrapper').toggleClass('active_wrapper');
+    });
+
 });
